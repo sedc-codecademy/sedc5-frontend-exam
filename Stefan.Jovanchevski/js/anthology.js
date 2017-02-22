33d17679f23d66ab644cd07ef819757328b663a8
stef4o <jovanchevskis@outlook.com>
2017-02-22 23:18:25 +0100

    Improvement in classes

--- Stefan.Jovanchevski/js/anthology.js
+++ Stefan.Jovanchevski/js/anthology.js
@@ -1,12 +1,6 @@
-function Anthology(id, title, editor, publisher, yearOfPublication, length, stories, ISBN, review) {
-    this.kind = "anthology";
-    this.id = id;
-    this.title = title;
-    this.editor = editor;
-    this.publisher = publisher;
-    this.yearOfPublication = yearOfPublication;
-    this.length = length;
-    this.stories = stories;
-    this.ISBN = ISBN;
-    this.review = review;
+class Anthology extends Book {
+    constructor(id, title, author, publisher, yearOfPublication, length, series, seriesNumber, ISBN, review) {
+        super(id, "novel", title, author, publisher, yearOfPublication, length, ISBN, review)
+        this.stories = stories;
+    }
 }
---
+++ Stefan.Jovanchevski/js/book.js
@@ -0,0 +1,13 @@
+class Book {
+    constructor(id, kind, title, author, publisher, yearOfPublication, length, ISBN, review) {
+        this.id = id;
+        this.kind = kind;
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.yearOfPublication = yearOfPublication;
+        this.length = length;
+        this.ISBN = ISBN;
+        this.review = review;
+    }
+}
