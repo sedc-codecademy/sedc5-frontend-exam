085dcf1cab5932e68d5d8bec70d7e095a313c913
stef4o <jovanchevskis@outlook.com>
2017-02-22 23:23:48 +0100

    Improvement in classes

--- Stefan.Jovanchevski/js/anthology.js
+++ Stefan.Jovanchevski/js/anthology.js
@@ -1,6 +1,6 @@
 class Anthology extends Book {
     constructor(id, title, author, publisher, yearOfPublication, length, series, seriesNumber, ISBN, review) {
-        super(id, "novel", title, author, publisher, yearOfPublication, length, ISBN, review)
+        super(id, "anthology", title, author, publisher, yearOfPublication, length, ISBN, review)
         this.stories = stories;
     }
 }
